b3dd1f78d35a64e0a5524eeb8b7acdeb5baf8c55
Jiaxi Ye <jiaxi@ucsb.edu>
2020-06-08 23:47:51 -0700

    Updates

---
+++ precache-manifest.a339eedfc4e941e83d974ab392afdb2e.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8c4d0526e1319c4011c5a3964f5f709",
+    "url": "/cs185-hw8/index.html"
+  },
+  {
+    "revision": "c42a30354c8f76edd148",
+    "url": "/cs185-hw8/static/css/2.b95c9955.chunk.css"
+  },
+  {
+    "revision": "1dc837a8da2b3bbef495",
+    "url": "/cs185-hw8/static/css/main.66f97f22.chunk.css"
+  },
+  {
+    "revision": "c42a30354c8f76edd148",
+    "url": "/cs185-hw8/static/js/2.8c6bddbb.chunk.js"
+  },
+  {
+    "revision": "4d1006dbdac151d7598a1a35010e25e6",
+    "url": "/cs185-hw8/static/js/2.8c6bddbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dc837a8da2b3bbef495",
+    "url": "/cs185-hw8/static/js/main.97553a10.chunk.js"
+  },
+  {
+    "revision": "93290b29701f47431275",
+    "url": "/cs185-hw8/static/js/runtime-main.c9627a01.js"
+  },
+  {
+    "revision": "d127a2bc9244b85a3f8842b141e3d85d",
+    "url": "/cs185-hw8/static/media/5_centimeters.d127a2bc.jpg"
+  },
+  {
+    "revision": "2028fa4e3171257e5c173243183af009",
+    "url": "/cs185-hw8/static/media/IMG_0898.2028fa4e.JPG"
+  },
+  {
+    "revision": "ce344b1bd955fe29523d8628e51818b9",
+    "url": "/cs185-hw8/static/media/IMG_2661.ce344b1b.GIF"
+  },
+  {
+    "revision": "7d7d94b0c7ad3be566d6dce3b5b6b6c7",
+    "url": "/cs185-hw8/static/media/IMG_6102.7d7d94b0.GIF"
+  },
+  {
+    "revision": "edd55572e07772a52d7d2d31a7d47851",
+    "url": "/cs185-hw8/static/media/IMG_7554.edd55572.GIF"
+  },
+  {
+    "revision": "a01903db5b4665267d0e4d0a2552936e",
+    "url": "/cs185-hw8/static/media/IMG_7555.a01903db.GIF"
+  },
+  {
+    "revision": "a062c5ba3aa66733f63470e92b295cdd",
+    "url": "/cs185-hw8/static/media/IMG_7562.a062c5ba.GIF"
+  },
+  {
+    "revision": "87d9db98153a2636260d89985fa77c8e",
+    "url": "/cs185-hw8/static/media/Owl-tries-on-hat.87d9db98.gif"
+  },
+  {
+    "revision": "2fa2ecd9b944e609dfefd4fd815f7459",
+    "url": "/cs185-hw8/static/media/angry_owl.2fa2ecd9.gif"
+  },
+  {
+    "revision": "fefbeab72b08f941d511e03e2ed6dd89",
+    "url": "/cs185-hw8/static/media/giphy.fefbeab7.gif"
+  },
+  {
+    "revision": "a1dd68bc8a23f685c3100ec4a13d0373",
+    "url": "/cs185-hw8/static/media/img.a1dd68bc.gif"
+  },
+  {
+    "revision": "c85f2cebf5c453af13334065d1cc2204",
+    "url": "/cs185-hw8/static/media/iphone-spinner-2.c85f2ceb.gif"
+  },
+  {
+    "revision": "51e0f364cb07e31d910edf7bcb6d04aa",
+    "url": "/cs185-hw8/static/media/leon.51e0f364.jpg"
+  },
+  {
+    "revision": "6bf797ad5fcdc0ce6f782bddefbd8869",
+    "url": "/cs185-hw8/static/media/star-sparse.6bf797ad.png"
+  },
+  {
+    "revision": "567a81a6940d4f21cbae84506957ed00",
+    "url": "/cs185-hw8/static/media/tenor.567a81a6.gif"
+  },
+  {
+    "revision": "10bf010897ece95bc5dc0b59e1b4bd3a",
+    "url": "/cs185-hw8/static/media/wandering_earth.10bf0108.jpg"
+  }
+]);
