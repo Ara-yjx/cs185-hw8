0fbec454898625ada142416955b90b84ce2634a0
Jiaxi Ye <jiaxi@ucsb.edu>
2020-06-08 23:41:13 -0700

    hw8 done

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            'page': 'Graph', // change me for easier debug
+            'page': 'Home', // change me for easier debug
             'overlay': 'hidden',
             'gototop': 'hidden',
             'overlayImg': require('./imgs/owls/img.gif'),
--- src/Graph.js
+++ src/Graph.js
@@ -16,27 +16,36 @@ export default class Graph extends Component {
         this.svgRef = React.createRef();
     }
 
-    chart = function(nodes, links) {
+    chart = function(nodes, links, patterns) {
 
         // Display
         const svg = d3.create('svg').attr('viewBox', [0,0,WIDTH,HEIGHT]);
+        var pattern = svg.append('defs')
+            .selectAll('pattern').data(patterns).join('pattern')
+            .attr('id', d => d.imdbid)
+            .attr('width', '1').attr('height', '1')
+            .attr('patternUnits', "objectBoundingBox")
+            .append('image')
+            .attr('href', d => d.poster)
+            .attr('width', '160')
+            .attr('x', 0).attr('y', 0)
+            
         var link = svg.append('g').attr('stroke', '#999')
             .selectAll('line').data(links).join('line')
-            .attr('stroke-width', '10')
+            .attr('stroke-width', '4')
+
         var node = svg.append('g').attr('stroke', '#999')
             .selectAll('circle').data(nodes).join('circle')
-            .attr('fill', '#333')
             .attr('r', d => d.r)
-            .attr('id', d => d.id)
-        console.log(d3.selectAll('circle'))
+            .attr('fill', d => d.imdbid ? 'url(#'+d.imdbid+')' : '#333')
+
         var text = svg.append('text').attr('id', 'svg-text')
-            .attr('stroke', '#333').attr('fill', '#fff')
-            // .attr('x', 1000).attr('y', 1000)
+            .attr('stroke', '#fff').attr('fill', '#000')
 
         // Force
         const simulation = d3.forceSimulation(nodes)
             .force('link', d3.forceLink().links(links).id(d => d.index).distance(300))
-            .force('charge', d3.forceManyBody().strength(-1000))
+            .force('charge', d3.forceManyBody().strength(-100))
             .force('center', d3.forceCenter(WIDTH/2, HEIGHT/2))
         simulation.on('tick', () => {
             link.attr('x1', d=>d.source.x)
@@ -70,13 +79,11 @@ export default class Graph extends Component {
 
         // Hover Display
         node.on('mouseover', d => {
-            let {x, y, id, text} = d
-            console.log(d3.select('#'+d.id))
+            let {x, y, text} = d
             d3.select('#svg-text').text(text)
                 .attr('x', x).attr('y', y)
         }).on('mouseout', d => {
-            console.log(d)
-            console.log('out')
+            d3.select('#svg-text').text('')
         })
 
         return svg.node();
@@ -85,21 +92,30 @@ export default class Graph extends Component {
 
     componentDidMount = function() {
         firebase.database().ref('movies').on('value', (snapshot)=>{
-            let movies = snapshot.val()['GraphViz']
-            movies = [
-                {Title: 'AAA', Actors:['a','b','c']},
-                {Title: 'BBB', Actors:['b','c']},
-                {Title: 'CCC', Actors:['c']},
-            ]
-            // Array of titles and actors text
+            let movies = snapshot.val()?.GraphViz
+            movies = Object.values(movies)
+            // break actors string into string list
+            const merge = list => Array.from(new Set([].concat.apply([], list)))
+            movies.forEach(e => e.ActorsList = merge(e.Actors?.replace(' ', '').split(',')));
+
             var titles = movies.map(e => e.Title)
-            var actors = Array.from(new Set([].concat.apply([], movies.map(e => e.Actors))))
+            var actors = merge(movies.map(e => e.ActorsList))
+            var patterns = movies.map(e => ({
+                poster: e.Poster,
+                imdbid: e.imdbID,
+            }))
             // Nodes and links
-            var nodes = titles.map((name, i) => ({text:name, r:100, id:'node-t-'+i}))
-                .concat(actors.map((name, i) => ({text:name, r:50, id:'node-a-'+i})))
+            var nodes = movies.map(movie => ({
+                text: movie.Title,
+                imdbid: movie.imdbID,
+                poster: movie.Poster,
+                r: 80,
+            })).concat(actors.map((name, i) => ({
+                text:name, r:30
+            })))
             var links = []
             for(let movie of movies) {
-                for(let actor of movie.Actors) {
+                for(let actor of movie.Actors?.replace(' ', '').split(',')) {
                     links.push({
                         source: titles.indexOf(movie.Title),
                         target: actors.indexOf(actor) + titles.length
@@ -107,7 +123,8 @@ export default class Graph extends Component {
                 }
             }
             // Make SVG and attach
-            this.svgRef.current.appendChild(this.chart(nodes, links));
+            if(this.svgRef.current)
+                this.svgRef.current.appendChild(this.chart(nodes, links, patterns));
         })
     }
 
